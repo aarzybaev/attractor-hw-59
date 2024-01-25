0fd5ddaa769ead3f18e328bf9ed3a0c8431d0a43
Askhad Arzybaev <aarzybaev@gmail.com>
2024-01-25 23:25:00 +0600

    created MovieForm component

---
+++ src/components/MovieForm/MovieForm.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {Movie, MovieForm} from '../../type';
+
+interface Props {
+  onSubmit: (movie: Movie) => void;
+}
+const MovieForm: React.FC<Props> = ({onSubmit}) => {
+  const [movieForm, setMovieForm] = useState<MovieForm>(
+    {title: ''}
+  );
+  const changeMovieForm = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setMovieForm(prevState => ({...prevState, [e.target.name]: e.target.value}));
+  };
+
+  const onFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit({
+      id: Math.random().toString(),
+      ...movieForm
+    });
+
+  };
+
+  return (
+    <form onSubmit={onFormSubmit} className="row justify-content-center">
+      <div className="col-4">
+        <input type="text" className="form-control" id="inputSearch" name="title" value={movieForm.title} onChange={changeMovieForm}/>
+      </div>
+      <div className="col-auto">
+        <button type="submit" className="btn btn-primary mb-3">Add</button>
+      </div>
+    </form>
+  );
+};
+
+export default MovieForm;
